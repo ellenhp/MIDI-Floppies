b828ea6fd65793f178e0d510ea70c2d3349e0a9a
Nolan Poe <nopoe.dev@gmail.com>
2014-06-17 13:14:36 -0700

    multi-channel playback enhancements and pitch bending

--- floppies/floppies.ino
+++ floppies/floppies.ino
@@ -10,14 +10,16 @@ unsigned int micros_per_tick=40;
 byte current_pos[NUM_FLOPPIES];
 byte current_dir[NUM_FLOPPIES];
 byte current_note[NUM_FLOPPIES];
+byte current_channel[NUM_FLOPPIES];
 unsigned int period_ticks[NUM_FLOPPIES];
 unsigned int ticks_since_step[NUM_FLOPPIES];
 byte last_cmd=0;
 
 void setupPins();
 void tick();
-void playNote(byte note, byte velocity);
-void stopNote(byte note);
+void playNote(byte channel, byte note, byte velocity);
+void stopNote(byte channel, byte note);
+void pitchBend(byte channel, byte LSB, byte MSB);
 void stepFloppy(byte floppy, byte dir);
 void resetAll();
 void pulseAll();
@@ -33,6 +35,8 @@ void setup() {
 
 void loop() {
   byte incomingByte;
+  byte msb;
+  byte lsb;
   byte note;
   byte velocity;
 
@@ -44,7 +48,7 @@ void loop() {
       note=SERIAL_READ();
       while (!SERIAL_AVAILABLE());
       velocity=SERIAL_READ();
-      playNote(note, velocity);
+      playNote(incomingByte-144, note, velocity);
       last_cmd=incomingByte;
     }
     else if (incomingByte >= 128 && incomingByte < 144) { // note off message starting
@@ -52,21 +56,35 @@ void loop() {
       note=SERIAL_READ();
       while (!SERIAL_AVAILABLE());
       SERIAL_READ();
-      stopNote(note);
+      stopNote(incomingByte-128, note);
       last_cmd=incomingByte;
-    } 
-    else if (incomingByte < 128 && last_cmd == 144) { // data byte, assume it's a continuation of the last command
+    }
+    else if (incomingByte >= 224 && incomingByte < 240) { // pitch bend message starting
+      while (!SERIAL_AVAILABLE());
+      lsb=SERIAL_READ();
+      while (!SERIAL_AVAILABLE());
+      msb=SERIAL_READ();
+      pitchBend(incomingByte-224, lsb, msb);
+      last_cmd=incomingByte;
+    }
+    else if (incomingByte < 128 && last_cmd >= 144 && last_cmd < 160) { // data byte, assume it's a continuation of the last command
       note=incomingByte;
       while (!SERIAL_AVAILABLE());
       velocity=SERIAL_READ();
-      playNote(note, velocity);
+      playNote(last_cmd-144, note, velocity);
     } 
-    else if (incomingByte < 128 && last_cmd == 128) { // data byte, assume it's a continuation of the last command
-      while (!SERIAL_AVAILABLE());
+    else if (incomingByte < 128 && last_cmd >= 128 && last_cmd < 144) { // data byte, assume it's a continuation of the last command
       note=incomingByte;
       while (!SERIAL_AVAILABLE());
       SERIAL_READ();
-      stopNote(note);
+      stopNote(last_cmd-128, note);
+    }
+    else if (incomingByte < 128 && incomingByte >= 224 && incomingByte < 240) { // data byte, assume it's a continuation of the last command
+      lsb=incomingByte;
+      while (!SERIAL_AVAILABLE());
+      msb=SERIAL_READ();
+      pitchBend(last_cmd-224, lsb, msb);
+      last_cmd=incomingByte;
     }
     else {
       // no other commands implemented
@@ -99,19 +117,20 @@ void tick() {
   }
 }
 
-void stopNote(byte note) {
+void stopNote(byte channel, byte note) {
   byte i;
   for (i=0; i<NUM_FLOPPIES; i++) {
-    if (current_note[i]==note) {
+    if (current_channel[i]==channel && current_note[i]==note) {
+      current_channel[i]=0;
       current_note[i]=0;
       period_ticks[i]=0;
     }
   }
 }
 
-void playNote(byte note, byte velocity) {
+void playNote(byte channel, byte note, byte velocity) {
   if (velocity==0) {
-    stopNote(note);
+    stopNote(channel, note);
     return;
   }
   byte floppy;
@@ -137,10 +156,45 @@ void playNote(byte note, byte velocity) {
     // set the values indicating what the floppy is doing
     period_ticks[desiredIndex]=period;
     current_note[desiredIndex]=note;
+    current_channel[desiredIndex]=channel;
 
     // step it now to get the note started ASAP
     stepFloppy(desiredIndex, current_dir[desiredIndex]);
     ticks_since_step[desiredIndex]=0;
+  }
+}
+
+void pitchBend(byte channel, byte LSB, byte MSB) {
+  byte i;
+  unsigned int finalVal=(((unsigned int)MSB & 0x7F) << 7) + ((unsigned int)LSB & 0x7F);
+  if (finalVal==0x2000) {
+    for (i=0; i<NUM_FLOPPIES; i++) {
+      if (current_channel[i]==channel) {
+        period_ticks[i]=PERIOD_BY_NOTE(current_note[i])/micros_per_tick;
+      }
+    }
+  }
+  else if (finalVal<0x2000) {
+    for (i=0; i<NUM_FLOPPIES; i++) {
+      if (current_channel[i]==channel) {
+        unsigned long pitchBendProportion=0x2000-finalVal;
+        unsigned long flatPeriod=PERIOD_BY_NOTE(current_note[i]-1);
+        unsigned long period=PERIOD_BY_NOTE(current_note[i]);
+        period+=(unsigned long)((flatPeriod-period)*((double)pitchBendProportion/0x2000));
+        period_ticks[i]=period/micros_per_tick;
+      }
+    }
+  }
+  else if (finalVal>0x2000) {
+    for (i=0; i<NUM_FLOPPIES; i++) {
+      if (current_channel[i]==channel) {
+        unsigned long pitchBendProportion=finalVal-0x2000;
+        unsigned long sharpPeriod=PERIOD_BY_NOTE(current_note[i]+1);
+        unsigned long period=PERIOD_BY_NOTE(current_note[i]);
+        period-=(unsigned long)((period-sharpPeriod)*((double)pitchBendProportion/0x2000));
+        period_ticks[i]=period/micros_per_tick;
+      }
+    }
   }
 }
 
@@ -192,5 +246,10 @@ void testAll() {
     pulseAll();
   }
 }
+
+
+
+
+
 
 
